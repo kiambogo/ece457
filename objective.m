88eca8b98863472388934f7dd9dcc42d7d1142c9
Josiah Witt <jdwitt@uwaterloo.ca>
2015-07-10 16:42:05 -0400

    power based objective function

--- objective.m
+++ objective.m
@@ -1,32 +1,22 @@
 % Objective function to evaluate a training plan
 
-% user_fitness = [fitness fatigue endurance climbing sprinting]
-% user_traits = [endurance_threshold]
-% user_goals = [total_fitness endurance climbing sprinting]
+% user_traits = [height mass c_rr c_d]
 
 % Returns the fitness of a training plan
 
-function fitness = objective(training_plan, user_fitness, user_traits, user_goals, L_fit, L_end, L_cli, L_spr)
-    global endur length L_fitness L_endurance L_climbing L_sprinting;
+function fitness = objective(training_plan, user_fitness, user_traits)
+    global length height mass c_rr;
     
-    endur = user_traits(1);
     length = size(training_plan);
-    L_fitness = L_fit;
-    L_endurance = L_end;
-    L_climbing = L_cli;
-    L_sprinting = L_spr;
+    height = user_traits(1);
+    mass = user_traits(2);
+    c_rr = user_traits(3); %https://en.wikipedia.org/wiki/Rolling_resistance
+    c_d = user_traits(4); %https://en.wikipedia.org/wiki/Drag_coefficient
     
     for i=1:size(training_plan)
-        fitness = fitness + g(training_plan(i,:), user_goals);
+        fitness = fitness + (2.75 * training_plan(i,2));
     end
-    fitness = fitness - h(training_plan) - q(training_plan, user_fitness);
-    
-    function y = g(x, goals)
-        endurance_points = heaviside(w((x(1)-endur)*x(2)/x(1)));
-        climbing_points = w((100/5)*x(3)*x(2)/x(1));
-        sprinting_points = w(x(2)/10);
-        y = goals(1)*w(x(2)) + goals(2)*endurance_points + goals(3)*climbing_points + goals(4)*sprinting_points;
-    end
+    fitness = fitness - H(training_plan) - Q(training_plan, user_fitness);
 
     function y = heaviside(X)
         %heaviside step function
@@ -35,40 +25,47 @@ function fitness = objective(training_plan, user_fitness, user_traits, user_goal
         y(X == 0) = .5;
     end
 
-    %calculate estimated effort for an activity
-    function y = w(x_time)
-        %0.1*1*x_time + 0.3*2*x_time + 0.4*3*x_time + 0.15*4*x_time + 0.05*5*x_time
-        y = 2.75*x_time;
-    end
-
-    function y = h(X)
+    function h = H(X)
         recovery_time = 0;
         for j=1:size(X)
-            recovery_time = recovery_time + u(X(j,:));
+            recovery_time = recovery_time + (2.75 * X(j,2) / 100);
         end
-        y = 100 * heaviside(recovery_time - length);
+        h = 100 * heaviside(recovery_time - length);
     end
 
-    function y = u(x)
-        y = g(x, [1 0 0 0])/100;
-    end
-
-    function y = q(X, p)
+    function q = Q(X, user_fitness)
         total_levels = 0;
         for j=1:size(X)
-           total_levels = total_levels + z(x, p);
+           total_levels = total_levels + L(x) - user_fitness;
         end
-        y = heaviside(-1000*total_levels);
+        q = heaviside(-1000 * total_levels);
     end
 
-    function y = z(x, p)
-        endurance = heaviside((x(1)-endur)/x(1))*v(L_endurance, x, p(3));
-        climbing = ((100/5)*x(3)/x(1))*v(L_climbing, x, p(4));
-        sprinting = v(L_sprinting, x, p(5))/10;
-        y = v(L_fitness, x, p(1)) + endurance + climbing + sprinting;
-    end
-
-    function y = v(L, x, p)
-        y = L(x(1), x(2), x(3)) - p;
+    %https://strava.zendesk.com/entries/20959332-Power-Calculations
+    function lvl = L(x)
+        d = x(1);
+        t = x(2);
+        e = x(3);
+        v = (d/1000)/(t/60);
+        grade = sin(e/d/2);
+        %https://en.wikipedia.org/wiki/Normal_force
+        N = arccos(grade) / (mass * 9.8);
+        %http://en.wikipedia.org/wiki/Rolling_resistance
+        p_rr = c_rr * N * v; 
+        %https://en.wikipedia.org/wiki/Density_of_air
+        ro = 1.2041;
+        %http://www.medcalc.com/body.html
+        a = (height * mass / 3600)^0.5; 
+        %http://en.wikipedia.org/wiki/Drag_(physics)
+        p_wind = 0.5 * ro * v^3 * c_d * a; 
+        p_g = mass * 9.8 * sin(arctan(grade)) * v;
+        p = p_rr + p_wind + p_g;
+        
+        if p < 2.5 || t <= 0
+            lvl = 0;
+        else
+            lvl_sc = p*t^(1/3);
+            lvl = 10*(lvl_sc - 2.5) + 1;
+        end
     end
 end
