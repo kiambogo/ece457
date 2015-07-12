1e196e93a4d5ae49dc27971d466ab5e977688290
Christopher Poenaru <kiambogo@gmail.com>
2015-07-12 15:20:33 -0400

    fixed a bunch of shit with the objective function

--- objective.m
+++ objective.m
@@ -7,7 +7,8 @@
 function fitness = objective(training_plan, user_fitness, user_traits)
     global length height mass c_rr;
     
-    length = size(training_plan);
+    %length = size(training_plan);
+    length = 14;
     height = user_traits(1);
     mass = user_traits(2);
     %https://en.wikipedia.org/wiki/Rolling_resistance, typically about 0.03
@@ -19,13 +20,15 @@ function fitness = objective(training_plan, user_fitness, user_traits)
     for i=1:size(training_plan)
         fitness = fitness + (2.75 * training_plan(i,2));
     end
-    fitness = fitness - H(training_plan) - Q(training_plan, user_fitness);
+    h = H(training_plan)
+    q = Q(training_plan, user_fitness)
+    fitness = fitness - h - q;
 
     function y = heaviside(X)
         %heaviside step function
         y = zeros(size(X));
-        y(X > 0) = 1;
-        y(X == 0) = .5;
+        y(X > 0) = 1 * X;
+        y(X == 0) = .5 * X;
     end
 
     function h = H(X)
@@ -39,9 +42,10 @@ function fitness = objective(training_plan, user_fitness, user_traits)
     function q = Q(X, user_fitness)
         total_levels = 0;
         for j=1:size(X)
-           total_levels = total_levels + L(X) - user_fitness;
+            l = L(X(j,:));
+           total_levels = total_levels + l - user_fitness
         end
-        q = heaviside(-1000 * total_levels);
+        q = heaviside(1000 * total_levels);
     end
 
     function lvl = L(x)
